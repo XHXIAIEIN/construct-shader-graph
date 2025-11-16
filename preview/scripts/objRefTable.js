66fcb09128066815cebc3b6d14938e5e3b44fd0b
Ossama Jouini <skymen75019@gmail.com>
2025-11-17 00:39:07 +0100

    Allow dropping textures

--- preview/scripts/objRefTable.js
+++ preview/scripts/objRefTable.js
@@ -5,7 +5,24 @@ self.C3_GetObjectRefTable = function () {
 		C3.Plugins.TiledBg,
 		C3.Plugins.Shape3D,
 		C3.Plugins.Camera3D,
-		C3.Plugins.Mouse
+		C3.Plugins.Mouse,
+		C3.Plugins.FileSystem,
+		C3.Plugins.Arr,
+		C3.Plugins.BinaryData,
+		C3.Plugins.Browser,
+		C3.Plugins.FileSystem.Cnds.OnDroppedFiles,
+		C3.Plugins.Arr.Acts.SplitString,
+		C3.Plugins.Browser.Acts.ConsoleLog,
+		C3.Plugins.FileSystem.Exps.FileNameAt,
+		C3.Plugins.System.Exps.tokenat,
+		C3.Plugins.System.Exps.tokencount,
+		C3.Plugins.Arr.Cnds.Contains,
+		C3.Plugins.FileSystem.Acts.ReadBinaryFile,
+		C3.Plugins.FileSystem.Cnds.OnFileOperationComplete,
+		C3.Plugins.Sprite.Cnds.IsVisible,
+		C3.Plugins.Sprite.Acts.LoadURL,
+		C3.Plugins.BinaryData.Exps.GetURL,
+		C3.Plugins.Shape3D.Cnds.IsVisible
 	];
 };
 self.C3_JsPropNameTable = [
@@ -14,7 +31,12 @@ self.C3_JsPropNameTable = [
 	{shape3d: 0},
 	{camera: 0},
 	{background3d: 0},
-	{Mouse: 0}
+	{Mouse: 0},
+	{FileSystem: 0},
+	{ValidImageFileFormats: 0},
+	{BinaryData: 0},
+	{shape3dTexture: 0},
+	{Browser: 0}
 ];
 
 self.InstanceType = {
@@ -23,5 +45,10 @@ self.InstanceType = {
 	shape3d: class extends self.I3DShapeInstance {},
 	camera: class extends self.IInstance {},
 	background3d: class extends self.I3DShapeInstance {},
-	Mouse: class extends self.IInstance {}
+	Mouse: class extends self.IInstance {},
+	FileSystem: class extends self.IInstance {},
+	ValidImageFileFormats: class extends self.IArrayInstance {},
+	BinaryData: class extends self.IBinaryDataInstance {},
+	shape3dTexture: class extends self.ISpriteInstance {},
+	Browser: class extends self.IInstance {}
 }
